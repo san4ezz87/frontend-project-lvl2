c7d621b49f9f468907027943249348b09fb5b0f4
Alex <san4ezz.8989@gmail.com>
2020-08-28 11:59:19 +0300

    add new optio -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,6 @@ const program = new Command();
 program.version('0.0.1', '-v, --vers', 'output the current version')
 .description('Usage: gendiff [options]')
 .description('Compares two configuration files and shows a difference.')
+.option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
